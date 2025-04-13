750f761c05335e4aa1c3d490c8781221cd055170
MeghnaSuryas18 <meghnasuryas17@gmail.com>
2025-04-13 21:52:04 +0530

    Delete js directory

--- js/cartFlow.js
+++
@@ -1,7 +0,0 @@
-function cartFlow(){
-    if (sessionStorage.getItem("cart") === null) {
-        window.location.href = "cart-empty.html";
-    } else {
-        window.location.href = "cart.html" ;
-   }
-}
